dac14296be574e6e51d413d4a21d8c14d17e49eb
AARIYAN AJIM DEVELOPER <140634500+ARIYAN-AJIM@users.noreply.github.com>
2023-12-04 09:50:00 +0000

    conversionInJavascript

---
+++ 01-basics/01conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = false;
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let isLogedIn = 1
+
+// let booleanIsLoggedIn = isLogedIn
+
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log( stringNumber)
+console.log(typeof stringNumber)
